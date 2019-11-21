162c1b342f0e3c63546dc2313104e0f07f1c9b2f
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2019-11-20 17:52:46 -0800

    Added php files instead of html to make a nicer more organized file system

---
+++ php/header.php
@@ -0,0 +1,28 @@
+<head>
+      <title>Hunters Web Desings &amp; Bot Development</title>
+      <meta charset="UTF-8">
+      <meta name="description" content="Hunter's Web Designs & Bot Development Hosting Services">
+      <meta name="keywords" content="hunter's web designs, hosting, creative, html, css, js">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Favicon -->
+      <link href="img/favicon.ico" rel="shortcut icon"/>
+      <!-- Google Font -->
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+      <script src="https://kit.fontawesome.com/705de00ff8.js" crossorigin="anonymous"></script>
+      <!-- Stylesheets -->
+      <link rel="stylesheet" href="css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="css/font-awesome.min.css"/>
+      <link rel="stylesheet" href="css/flaticon.css"/>
+      <link rel="stylesheet" href="css/owl.carousel.min.css"/>
+      <link rel="stylesheet" href="css/animate.css"/>
+      <link rel="stylesheet" href="css/style.css"/>
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <style>
+         .fa-discord{
+         font-size: 1rem;
+         }
+      </style>
+   </head>
