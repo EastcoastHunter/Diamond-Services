b3ec1d7a21c51004c3729bd845ab95fb2143eb09
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2019-11-20 18:49:48 -0800

    added new html page for development chances and a new meta-tags.php page

---
+++ php/meta-tags.php
@@ -0,0 +1,8 @@
+<meta name="title" content="Hunter's Web Designs &amp; Bot Development">
+    <meta name="description" content="Premium Discord Bot Hosting and Web Hosting &amp; Web Designing">
+    <meta name="keywords" content="discord bot's, webhosting, premium quality">
+    <meta name="robots" content="noindex, nofollow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="31 days">
+    <meta name="author" content="Hunter L.">
