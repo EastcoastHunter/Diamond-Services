b161de398a3224e25c9496b8efbc0f1f689ab3bb
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2019-11-20 19:03:24 -0800

    Added Image To Emedable Discription

--- php/meta-tags.php
+++ php/meta-tags.php
@@ -1,8 +1,15 @@
 <meta name="title" content="Hunter's Web Designs &amp; Bot Development">
-    <meta name="description" content="Premium Discord Bot Hosting and Web Hosting &amp; Web Designing">
-    <meta name="keywords" content="discord bot's, webhosting, premium quality">
-    <meta name="robots" content="noindex, nofollow">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="English">
-    <meta name="revisit-after" content="31 days">
-    <meta name="author" content="Hunter L.">
+   <meta name="description" content="Premium Discord Bot Hosting and Web Hosting &amp; Web Designing">
+   <meta name="keywords" content="discord bot's, webhosting, premium quality">
+   <meta name="robots" content="noindex, nofollow">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <meta name="language" content="English">
+   <meta name="revisit-after" content="31 days">
+   <meta name="author" content="Hunter L.">
+   <!-- Image Meta Tags -->
+   <meta name="og:title" content="Hunter's Web Designs" />
+   <meta name="og:type" content="Image" />
+   <meta name="og:url" content="https://ownerhunter.github.io/Diamond-Services/index" />
+   <meta name="og:image" content="./img/favicon.ico" />
+   <meta name="og:site_name" content="Hunter's Web Designs & Bot Development" />
+   <meta name="og:description" content="Premium Discord Bot Hosting and Web Hosting &amp; Web Designing" />
